a1d69dbba7072991dae16964ad61bc70128d982a
Feross Aboukhadijeh <feross@feross.org>
2016-01-02 16:01:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# WebTorrent.app
+
+### Streaming torrent client, for the desktop
+
+## Dev
+
+```
+$ npm install
+```
+
+### Run
+
+```
+$ npm start
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+Builds the app for OS X, Linux, and Windows, using [electron-packager](https://github.com/maxogden/electron-packager).
+
+
+## License
+
+MIT. Copyright (c) [Feross Aboukhadijeh](http://feross.org).
+
---
+++ index.js
@@ -0,0 +1,45 @@
+var electron = require('electron')
+var app = electron.app
+
+// report crashes to the Electron project
+require('crash-reporter').start()
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')()
+
+// prevent window being garbage collected
+var mainWindow
+
+function onClosed () {
+  // dereference the window
+  // for multiple windows store them in an array
+  mainWindow = null
+}
+
+function createMainWindow () {
+  const win = new electron.BrowserWindow({
+    width: 600,
+    height: 400
+  })
+
+  win.loadURL('file://' + __dirname + '/index.html')
+  win.on('closed', onClosed)
+
+  return win
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (!mainWindow) {
+    mainWindow = createMainWindow()
+  }
+})
+
+app.on('ready', () => {
+  mainWindow = createMainWindow()
+})
