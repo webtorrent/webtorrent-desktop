e7d837e813ab3f88ed8ad8ca21142c92f0e1b790
Feross Aboukhadijeh <feross@feross.org>
2016-03-09 15:05:39 -0800

    shortcut order

--- main/shortcuts.js
+++ main/shortcuts.js
@@ -7,11 +7,11 @@ var globalShortcut = electron.globalShortcut
 var localShortcut = require('electron-localshortcut')
 
 function init (menu, windows) {
+  // Special "media key" for play/pause, available on some keyboards
+  globalShortcut.register('MediaPlayPause', () => windows.main.send('dispatch', 'playPause'))
+
   // ⌘+Shift+F is an alternative fullscreen shortcut to the ones defined in menu.js.
   // Electron does not support multiple accelerators for a single menu item, so this
   // is registered separately here.
   localShortcut.register('CmdOrCtrl+Shift+F', menu.toggleFullScreen)
-
-  // Special "media key" for play/pause, available on some keyboards
-  globalShortcut.register('MediaPlayPause', () => windows.main.send('dispatch', 'playPause'))
 }
