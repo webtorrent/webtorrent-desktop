8ae727db035630bf56609f069966370332da67ed
Feross Aboukhadijeh <feross@feross.org>
2016-03-20 03:29:02 -0700

    rely on electron's automatic ASAR handling

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var path = require('path')
 
 module.exports = {
   APP_COPYRIGHT: 'Copyright © 2014-2016 The WebTorrent Project',
-  APP_FILE_ICON: path.join(pathToStatic(), 'WebTorrentFile'),
-  APP_ICON: path.join(pathToStatic(), 'WebTorrent'),
+  APP_FILE_ICON: path.join(__dirname, 'static', 'WebTorrentFile'),
+  APP_ICON: path.join(__dirname, 'static', 'WebTorrent'),
   APP_NAME: 'WebTorrent',
 
   CONFIG_PATH: applicationConfigPath('WebTorrent'),
@@ -15,7 +15,7 @@ module.exports = {
 
   IS_PRODUCTION: isProduction(),
 
-  STATIC_PATH: pathToStatic(),
+  STATIC_PATH: path.join(__dirname, 'static'),
 
   SOUND_ADD: 'file://' + path.join(__dirname, 'static', 'sound', 'add.wav'),
   SOUND_DELETE: 'file://' + path.join(__dirname, 'static', 'sound', 'delete.wav'),
@@ -40,10 +40,4 @@ function isProduction () {
   if (process.platform === 'linux') {
     return !/\/electron$/.test(process.execPath)
   }
-}
-
-function pathToStatic () {
-  return isProduction()
-    ? path.join(process.resourcesPath, 'app.asar.unpacked', 'static')
-    : path.join(__dirname, 'static')
 }
