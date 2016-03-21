3ba4b563ac272185469bbcfe0e038f89f4c8ed2c
Feross Aboukhadijeh <feross@feross.org>
2016-03-21 01:10:00 -0700

    move crash reporter placeholder

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 console.time('init')
 require('./main')
-
-// report crashes
-// require('crash-reporter').start({
-//   productName: 'WebTorrent',
-//   companyName: 'WebTorrent',
-//   submitURL: 'https://webtorrent.io/crash-report',
-//   autoSubmit: true
-// })
