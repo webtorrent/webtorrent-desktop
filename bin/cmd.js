1f9551bd0ab38063240c53d8f38d07591e077748
Feross Aboukhadijeh <feross@feross.org>
2016-03-27 23:47:32 -0700

    Fixes for PR #250

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 var electron = require('electron-prebuilt')
-var proc = require('child_process')
+var cp = require('child_process')
 var path = require('path')
 
-var child = proc.spawn(electron, [path.join(__dirname, '..')], {stdio: 'inherit'})
+var child = cp.spawn(electron, [path.join(__dirname, '..')], {stdio: 'inherit'})
 child.on('close', function (code) {
   process.exit(code)
 })
