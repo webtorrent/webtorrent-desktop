b39566fcd29f122d8fe6048383cdb16abea25dca
DC <dcposch@dcpos.ch>
2016-05-04 04:33:35 -0700

    External VLC on Windows
    Turns out we can't use vlc --version because it pops up a command prompt :/

--- main/vlc.js
+++ main/vlc.js
@@ -1,33 +1,15 @@
 module.exports = {
-  getInstalledVersion,
+  checkForVLC,
   spawn
 }
 
 var cp = require('child_process')
 var vlcCommand = require('vlc-command')
 
-// Runs vlc --version. Calls back with the currently installed version of VLC
-// or null if VLC is not installed. (Or 'unknown' if VLC runs and produces bad
-// output, but that should never happen.)
-function getInstalledVersion (cb) {
-  exec(['--version'], function (e, stdout) {
-    var version
-    if (e) {
-      version = null
-    } else {
-      // Prints several lines, starting with eg: VLC media player 2.7.0
-      if (!stdout.startsWith('VLC media player')) version = 'unknown'
-      else version = stdout.split(' ')[3]
-    }
-    cb(version)
-  })
-}
-
-function exec (args, cb) {
-  vlcCommand(function (err, vlcPath) {
-    if (err) return cb(err)
-    cp.execFile(vlcPath, args, cb)
-  })
+// Finds if VLC is installed on Mac, Windows, or Linux.
+// Calls back with true or false: whether VLC was detected
+function checkForVLC (cb) {
+  vlcCommand((err) => cb(!err))
 }
 
 // Finds if VLC is installed on Mac, Windows, or Linux.
