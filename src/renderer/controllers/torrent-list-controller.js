webtorrent/webtorrent-desktop

commit 0809e20a6e1de917a2f8315c2f1da3b9ef177a43

Check path for each torrent

src/renderer/controllers/torrent-list-controller.js
@@ -76,21 +76,25 @@ module.exports = class TorrentListController {
   }
 
   // Starts downloading and/or seeding a given torrentSummary.
-  startTorrentingSummary (torrentSummary) {
-    var s = torrentSummary
-
-    // Backward compatibility for config files save before we had torrentKey
-    if (!s.torrentKey) s.torrentKey = this.state.nextTorrentKey++
+  startTorrentingSummary (torrentKey) {
+    var s = TorrentSummary.getByKey(this.state, torrentKey)
+    if (!s) throw new Error('Missing key: ' + torrentKey)
 
     // Use Downloads folder by default
     if (!s.path) s.path = this.state.saved.prefs.downloadPath
 
-    ipcRenderer.send('wt-start-torrenting',
-                      s.torrentKey,
-                      TorrentSummary.getTorrentID(s),
-                      s.path,
-                      s.fileModtimes,
-                      s.selections)
+    fs.stat(TorrentSummary.getFileOrFolder(s), function (err) {
+      if (err) {
+        s.error = 'path-missing'
+        return
+      }
+      ipcRenderer.send('wt-start-torrenting',
+        s.torrentKey,
+        TorrentSummary.getTorrentID(s),
+        s.path,
+        s.fileModtimes,
+        s.selections)
+    })
   }
 
   // TODO: use torrentKey, not infoHash
@@ -98,7 +102,7 @@ module.exports = class TorrentListController {
     var torrentSummary = TorrentSummary.getByKey(this.state, infoHash)
     if (torrentSummary.status === 'paused') {
       torrentSummary.status = 'new'
-      this.startTorrentingSummary(torrentSummary)
+      this.startTorrentingSummary(torrentSummary.torrentKey)
       sound.play('ENABLE')
     } else {
       torrentSummary.status = 'paused'

src/renderer/controllers/playback-controller.js
@@ -188,7 +188,7 @@ module.exports = class PlaybackController {
     }, 10000) /* give it a few seconds */
 
     if (torrentSummary.status === 'paused') {
-      dispatch('startTorrentingSummary', torrentSummary)
+      dispatch('startTorrentingSummary', torrentSummary.torrentKey)
       ipcRenderer.once('wt-ready-' + torrentSummary.infoHash,
         () => this.openPlayerFromActiveTorrent(torrentSummary, index, timeout, cb))
     } else {

src/renderer/lib/state.js
@@ -200,6 +200,9 @@ function save (state, cb) {
         if (key === 'playStatus') {
           continue // Don't save whether a torrent is playing / pending
         }
+        if (key === 'error') {
+          continue // Don't save error states
+        }
         torrent[key] = x[key]
       }
       return torrent

src/renderer/main.js
@@ -75,14 +75,14 @@ function onState (err, _state) {
     }
   })
 
+  // Restart everything we were torrenting last time the app ran
+  resumeTorrents()
+
   // Calling update() updates the UI given the current state
   // Do this at least once a second to give every file in every torrentSummary
   // a progress bar and to keep the cursor in sync when playing a video
   setInterval(update, 1000)
   app = ReactDOM.render(<App state={state} />, document.querySelector('#body'))
-
-  // Restart everything we were torrenting last time the app ran
-  resumeTorrents()
 
   // Lazy-load other stuff, like the AppleTV module, later to keep startup fast
   window.setTimeout(delayedInit, config.DELAYED_INIT)
@@ -179,8 +179,7 @@ const dispatchHandlers = {
   'deleteTorrent': (infoHash, deleteData) => controllers.torrentList.deleteTorrent(infoHash, deleteData),
   'toggleSelectTorrent': (infoHash) => controllers.torrentList.toggleSelectTorrent(infoHash),
   'openTorrentContextMenu': (infoHash) => controllers.torrentList.openTorrentContextMenu(infoHash),
-  'startTorrentingSummary': (torrentSummary) =>
-    controllers.torrentList.startTorrentingSummary(torrentSummary),
+  'startTorrentingSummary': (torrentKey) => controllers.torrentList.startTorrentingSummary(torrentKey),
 
   // Playback
   'playFile': (infoHash, index) => controllers.playback.playFile(infoHash, index),
@@ -318,8 +317,14 @@ function escapeBack () {
 // Starts all torrents that aren't paused on program startup
 function resumeTorrents () {
   state.saved.torrents
-    .filter((torrentSummary) => torrentSummary.status !== 'paused')
-    .forEach((torrentSummary) => controllers.torrentList.startTorrentingSummary(torrentSummary))
+    .map((torrentSummary) => {
+      // Torrent keys are ephemeral, reassigned each time the app runs.
+      // On startup, give all torrents a key, even the ones that are paused.
+      torrentSummary.torrentKey = state.nextTorrentKey++
+      return torrentSummary
+    })
+    .filter((s) => s.status !== 'paused')
+    .forEach((s) => controllers.torrentList.startTorrentingSummary(s.torrentKey))
 }
 
 // Set window dimensions to match video dimensions or fill the screen
@@ -440,7 +445,6 @@ function onFullscreenChanged (e, isFullScreen) {
 }
 
 function checkDownloadPath () {
-  state.downloadPathStatus = undefined
   fs.stat(state.saved.prefs.downloadPath, function (err, stat) {
     if (err) {
       state.downloadPathStatus = 'missing'

src/renderer/views/torrent-list.js
@@ -9,31 +9,27 @@ module.exports = class TorrentList extends React.Component {
   render () {
     var state = this.props.state
 
-    var contents
-    if (!state.downloadPathStatus) {
-      contents = ''
-    } else if (state.downloadPathStatus === 'missing') {
-      contents = (
-        <div>
+    var contents = []
+    if (state.downloadPathStatus === 'missing') {
+      contents.push(
+        <div key='torrent-missing-path'>
           <p>Download path missing: {state.saved.prefs.downloadPath}</p>
           <p>Check that all drives are connected?</p>
-          <p>Alternatively, choose a new download path in
-            <a href='#' onClick={dispatcher('preferences')}>Preferences</a>
+          <p>Alternatively, choose a new download path
+            in <a href='#' onClick={dispatcher('preferences')}>Preferences</a>
           </p>
         </div>
       )
-    } else if (state.downloadPathStatus === 'ok') {
-      contents = state.saved.torrents.map(
-        (torrentSummary) => this.renderTorrent(torrentSummary)
-      )
-      contents.push(
-        <div key='torrent-placeholder' className='torrent-placeholder'>
-          <span className='ellipsis'>Drop a torrent file here or paste a magnet link</span>
-        </div>
-      )
-    } else {
-      throw new Error('Unhandled downloadPathStatus ' + state.downloadPathStatus)
     }
+    var torrentElems = state.saved.torrents.map(
+      (torrentSummary) => this.renderTorrent(torrentSummary)
+    )
+    contents.push(...torrentElems)
+    contents.push(
+      <div key='torrent-placeholder' className='torrent-placeholder'>
+        <span className='ellipsis'>Drop a torrent file here or paste a magnet link</span>
+      </div>
+    )
 
     return (
       <div key='torrent-list' className='torrent-list'>
@@ -64,7 +60,7 @@ module.exports = class TorrentList extends React.Component {
     if (torrentSummary.playStatus) classes.push(torrentSummary.playStatus)
     if (isSelected) classes.push('selected')
     if (!infoHash) classes.push('disabled')
-    if (torrentSummary.torrrentKey) console.error('Missing torrentKey', torrentSummary)
+    if (!torrentSummary.torrentKey) throw new Error('Missing torrentKey')
     return (
       <div
         key={torrentSummary.torrentKey}
@@ -88,8 +84,14 @@ module.exports = class TorrentList extends React.Component {
 
     // If it's downloading/seeding then show progress info
     var prog = torrentSummary.progress
-    if (torrentSummary.status !== 'paused' && prog) {
-      elements.push((
+    if (torrentSummary.error) {
+      elements.push(
+        <div key='progress-info' className='ellipsis'>
+          {getErrorMessage(torrentSummary)}
+        </div>
+      )
+    } else if (torrentSummary.status !== 'paused' && prog) {
+      elements.push(
         <div key='progress-info' className='ellipsis'>
           {renderPercentProgress()}
           {renderTotalProgress()}
@@ -98,7 +100,7 @@ module.exports = class TorrentList extends React.Component {
           {renderUploadSpeed()}
           {renderEta()}
         </div>
-      ))
+      )
     }
 
     return (<div key='metadata' className='metadata'>{elements}</div>)
@@ -195,8 +197,9 @@ module.exports = class TorrentList extends React.Component {
     }
 
     // Only show the play button for torrents that contain playable media
-    var playButton
-    if (TorrentPlayer.isPlayableTorrentSummary(torrentSummary)) {
+    var playButton, downloadButton
+    var noErrors = !torrentSummary.error
+    if (noErrors && TorrentPlayer.isPlayableTorrentSummary(torrentSummary)) {
       playButton = (
         <i
           key='play-button'
@@ -207,11 +210,8 @@ module.exports = class TorrentList extends React.Component {
         </i>
       )
     }
-
-    return (
-      <div key='buttons' className='buttons'>
-        {positionElem}
-        {playButton}
+    if (noErrors) {
+      downloadButton = (
         <i
           key='download-button'
           className={'button-round icon download ' + torrentSummary.status}
@@ -219,6 +219,14 @@ module.exports = class TorrentList extends React.Component {
           onClick={dispatcher('toggleTorrent', infoHash)}>
           {downloadIcon}
         </i>
+      )
+    }
+
+    return (
+      <div key='buttons' className='buttons'>
+        {positionElem}
+        {playButton}
+        {downloadButton}
         <i
           key='delete-button'
           className='icon delete'
@@ -233,12 +241,26 @@ module.exports = class TorrentList extends React.Component {
   // Show files, per-file download status and play buttons, and so on
   renderTorrentDetails (torrentSummary) {
     var filesElement
-    if (!torrentSummary.files) {
-      // We don't know what files this torrent contains
-      var message = torrentSummary.status === 'paused'
-        ? 'Failed to load torrent info. Click the download button to try again...'
-        : 'Downloading torrent info...'
-      filesElement = (<div key='files' className='files warning'>{message}</div>)
+    if (torrentSummary.error || !torrentSummary.files) {
+      var message = ''
+      if (torrentSummary.error === 'path-missing') {
+        // Special case error: this torrent's download dir or file is missing
+        message = 'Missing path: ' + TorrentSummary.getFileOrFolder(torrentSummary)
+      } else if (torrentSummary.error) {
+        // General error for this torrent: just show the message
+        message = torrentSummary.error.message || torrentSummary.error
+      } else if (torrentSummary.status === 'paused') {
+        // No file info, no infohash, and we're not trying to download from the DHT
+        message = 'Failed to load torrent info. Click the download button to try again...'
+      } else {
+        // No file info, no infohash, trying to load from the DHT
+        message = 'Downloading torrent info...'
+      }
+      filesElement = (
+        <div key='files' className='files warning'>
+          {message}
+        </div>
+      )
     } else {
       // We do know the files. List them and show download stats for each one
       var fileRows = torrentSummary.files
@@ -348,4 +370,17 @@ module.exports = class TorrentList extends React.Component {
       </div>
     )
   }
+}
+
+function getErrorMessage (torrentSummary) {
+  var err = torrentSummary.error
+  if (err === 'path-missing') {
+    return (
+      <span>
+        Path missing.<br />
+        Fix and restart the app, or delete the torrent.
+      </span>
+    )
+  }
+  return 'Error'
 }

static/main.css
@@ -554,8 +554,14 @@ input[type='text'] {
 /*
  * TORRENT LIST: ERRORS
  */
+
 .torrent-list p {
-  padding: 5px 20px;
+  margin: 10px 20px;
+}
+
+.torrent-list a {
+  color: #99f;
+  text-decoration: none;
 }
 
 /*

commit 6d4b8c3c26c7921eea8b1c7a288cbb5cb1fd660c

Fix playback + download of default torrents
There was a terrible bug introduced in https://github.com/feross/webtorrent-desktop/commit/0809e20a6e1de917a2f8315c2f1da3b9ef177a43 -- clicking play on any of the default torrents in a fresh install of the app would fail and result in a 'path missing' error.

This fixes the bug, and also adds a migration step to clean up resulting broken config files

src/renderer/controllers/torrent-list-controller.js
@@ -84,21 +84,30 @@ module.exports = class TorrentListController {
     var s = TorrentSummary.getByKey(this.state, torrentKey)
     if (!s) throw new Error('Missing key: ' + torrentKey)
 
-    // Use Downloads folder by default
-    if (!s.path) s.path = this.state.saved.prefs.downloadPath
+    // New torrent: give it a path
+    if (!s.path) {
+      // Use Downloads folder by default
+      s.path = this.state.saved.prefs.downloadPath
+      return start()
+    }
 
+    // Existing torrent: check that the path is still there
     fs.stat(TorrentSummary.getFileOrFolder(s), function (err) {
       if (err) {
         s.error = 'path-missing'
         return
       }
+      start()
+    })
+
+    function start () {
       ipcRenderer.send('wt-start-torrenting',
         s.torrentKey,
         TorrentSummary.getTorrentID(s),
         s.path,
         s.fileModtimes,
         s.selections)
-    })
+    }
   }
 
   // TODO: use torrentKey, not infoHash

src/renderer/lib/migrations.js
@@ -4,8 +4,10 @@ module.exports = {
   run
 }
 
-var semver = require('semver')
-var config = require('../../config')
+const semver = require('semver')
+const config = require('../../config')
+const TorrentSummary = require('./torrent-summary')
+const fs = require('fs')
 
 // Change `state.saved` (which will be saved back to config.json on exit) as
 // needed, for example to deal with config.json format changes across versions
@@ -110,4 +112,26 @@ function migrate_0_12_0 (saved) {
     saved.prefs.openExternalPlayer = saved.prefs.playInVlc
   }
   delete saved.prefs.playInVlc
+
+  // Undo a terrible bug where clicking Play on a default torrent on a fresh
+  // install results in a "path missing" error
+  // See https://github.com/feross/webtorrent-desktop/pull/806
+  var defaultTorrentFiles = [
+    '6a9759bffd5c0af65319979fb7832189f4f3c35d.torrent',
+    '88594aaacbde40ef3e2510c47374ec0aa396c08e.torrent',
+    '6a02592d2bbc069628cd5ed8a54f88ee06ac0ba5.torrent',
+    '02767050e0be2fd4db9a2ad6c12416ac806ed6ed.torrent',
+    '3ba219a8634bf7bae3d848192b2da75ae995589d.torrent'
+  ]
+  saved.torrents.forEach(function (torrentSummary) {
+    if (!defaultTorrentFiles.includes(torrentSummary.torrentFileName)) return
+    var fileOrFolder = TorrentSummary.getFileOrFolder(torrentSummary)
+    if (!fileOrFolder) return
+    try {
+      fs.statSync(fileOrFolder)
+    } catch (e) {
+      // Default torrent with "missing path" error. Clear path.
+      delete torrentSummary.path
+    }
+  })
 }