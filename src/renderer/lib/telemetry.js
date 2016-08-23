eba9aa3e17be669246d690529ee8b71ba6830486
DC <dcposch@dcpos.ch>
2016-08-22 23:57:00 -0700

    Telemetry: log app version

--- src/renderer/lib/telemetry.js
+++ src/renderer/lib/telemetry.js
@@ -24,6 +24,7 @@ function init (state) {
   }
 
   var now = new Date()
+  telemetry.version = config.APP_VERSION
   telemetry.timestamp = now.toISOString()
   telemetry.localTime = now.toTimeString()
   telemetry.screens = getScreenInfo()
@@ -33,7 +34,7 @@ function init (state) {
   if (config.IS_PRODUCTION) {
     postToServer()
     // If the user keeps WebTorrent running for a long time, post every 24h
-    setInterval(postToServer, 24 * 3600 * 1000)
+    setInterval(postToServer, 12 * 3600 * 1000)
   } else {
     // Development: telemetry used only for local debugging
     // Empty uncaught errors, etc at the start of every run
