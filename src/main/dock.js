43bb363ad23059e89b7439df6f3f7320bcef481b
Adam Gotlib <gotlib.adam+dev@gmail.com>
2016-09-01 12:30:27 +0200

    Fix error on Windows caused by `setBadge`

--- src/main/dock.js
+++ src/main/dock.js
@@ -4,9 +4,7 @@ module.exports = {
   setBadge
 }
 
-var electron = require('electron')
-
-var app = electron.app
+var {app, Menu} = require('electron')
 
 var dialog = require('./dialog')
 var log = require('./log')
@@ -16,7 +14,7 @@ var log = require('./log')
  */
 function init () {
   if (!app.dock) return
-  var menu = electron.Menu.buildFromTemplate(getMenuTemplate())
+  var menu = Menu.buildFromTemplate(getMenuTemplate())
   app.dock.setMenu(menu)
 }
 
@@ -33,8 +31,11 @@ function downloadFinished (path) {
  * Display a counter badge for the app. (Mac, Linux)
  */
 function setBadge (count) {
-  log(`setBadge: ${count}`)
-  app.setBadgeCount(Number(count))
+  if (process.platform === 'darwin' ||
+      process.platform === 'linux' && app.isUnityRunning()) {
+    log(`setBadge: ${count}`)
+    app.setBadgeCount(Number(count))
+  }
 }
 
 function getMenuTemplate () {
