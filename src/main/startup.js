0e6e8e95b98944a1300d25d758f6ab7651f7094e
Noam Okman <noamokman@gmail.com>
2016-09-24 08:51:46 +0300

    update auto-launch

--- src/main/startup.js
+++ src/main/startup.js
@@ -5,18 +5,13 @@ module.exports = {
 
 const config = require('../config')
 const AutoLaunch = require('auto-launch')
-const { app } = require('electron')
-
-// On Mac, work around a bug in auto-launch where it opens a Terminal window
-// See https://github.com/Teamwork/node-auto-launch/issues/28#issuecomment-222194437
-const appPath = process.platform === 'darwin'
-  ? app.getPath('exe').replace(/\.app\/Content.*/, '.app')
-  : undefined // Use the default
 
 const appLauncher = new AutoLaunch({
   name: config.APP_NAME,
-  path: appPath,
-  isHidden: true
+  isHidden: true,
+  mac: {
+    useLaunchAgent: true
+  }
 })
 
 function install () {
