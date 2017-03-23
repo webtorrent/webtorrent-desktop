996a81ddaa65b6933fed30b163a79eefa69987a7
Alberto Miranda <codealchemist@gmail.com>
2017-03-23 07:14:27 -0300

    added folder-watcher-controller

---
+++ src/renderer/controllers/folder-watcher-controller.js
@@ -0,0 +1,13 @@
+const {ipcRenderer} = require('electron')
+
+module.exports = class FolderWatcherController {
+  start () {
+    console.log('-- IPC: start folder watcher')
+    ipcRenderer.send('startFolderWatcher')
+  }
+
+  stop () {
+    console.log('-- IPC: stop folder watcher')
+    ipcRenderer.send('stopFolderWatcher')
+  }
+}
