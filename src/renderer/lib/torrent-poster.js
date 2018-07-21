cf8c1654e86086303a1de636cfd909aad96f5a8f
Borewit <borewit@users.noreply.github.com>
2018-07-21 14:10:23 +0200

    Add extension for MP4 audio book.

--- src/renderer/lib/torrent-poster.js
+++ src/renderer/lib/torrent-poster.js
@@ -4,7 +4,7 @@ const captureFrame = require('capture-frame')
 const path = require('path')
 
 const mediaExtensions = {
-  audio: ['.aac', '.asf', '.flac', '.m2a', '.m4a', '.mp2', '.mp4', '.mp3', '.oga', '.ogg', '.opus',
+  audio: ['.aac', '.asf', '.flac', '.m2a', '.m4a', '.m4b', '.mp2', '.mp4', '.mp3', '.oga', '.ogg', '.opus',
     '.wma', '.wav', '.wv', '.wvp'],
   video: ['.mp4', '.m4v', '.webm', '.mov', '.mkv'],
   image: ['.gif', '.jpg', '.jpeg', '.png']
