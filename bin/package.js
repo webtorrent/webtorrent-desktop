e8835c3054898a7fb64e4f2167bf216d9975fc67
Feross Aboukhadijeh <feross@feross.org>
2019-09-06 00:12:08 -0700

    package: Remove `npm dedupe`
    We don't want to modify the package-lock.json file at packaging time.

--- bin/package.js
+++ bin/package.js
@@ -39,7 +39,6 @@ const argv = minimist(process.argv.slice(2), {
 function build () {
   console.log('Reinstalling node_modules...')
   rimraf.sync(NODE_MODULES_PATH)
-  cp.execSync('npm dedupe', { stdio: 'inherit' })
   cp.execSync('npm ci', { stdio: 'inherit' })
 
   console.log('Nuking dist/ and build/...')
