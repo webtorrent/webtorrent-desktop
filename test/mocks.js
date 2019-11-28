83dd5e35a7996855cdd6243712fd7eaa1153a758
Julen Garcia Leunda <hicom150@gmail.com>
2019-11-28 17:13:37 +0100

    Fix integration tests

--- test/mocks.js
+++ test/mocks.js
@@ -4,9 +4,6 @@ const config = require('./config')
 console.log('Mocking electron.dialog.showOpenDialogSync...')
 electron.dialog.showOpenDialogSync = showOpenDialogSync
 
-console.log('Mocking electron.remote.dialog.showOpenDialogSync...')
-electron.remote.dialog.showOpenDialogSync = showOpenDialogSync
-
 function showOpenDialogSync (win, opts) {
   return /select.*torrent file/i.test(opts.title)
     ? config.TORRENT_FILES
@@ -15,9 +12,6 @@ function showOpenDialogSync (win, opts) {
 
 console.log('Mocking electron.dialog.showSaveDialogSync...')
 electron.dialog.showSaveDialogSync = showSaveDialogSync
-
-console.log('Mocking electron.remote.dialog.showSaveDialogSync...')
-electron.remote.dialog.showSaveDialogSync = showSaveDialogSync
 
 function showSaveDialogSync (win, opts) {
   return config.SAVED_TORRENT_FILE
