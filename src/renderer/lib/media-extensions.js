38aef814763360656aaca2c80d73d6c83cd279d7
Borewit <borewit@users.noreply.github.com>
2020-01-08 19:48:53 +0100

    Add additional audio file extensions: MPEG-Layer-2, Musepack, Matroska audio, WavePack

--- src/renderer/lib/media-extensions.js
+++ src/renderer/lib/media-extensions.js
@@ -1,8 +1,8 @@
 const mediaExtensions = {
   audio: [
     '.aac', '.aif', '.aiff', '.asf', '.dff', '.dsf', '.flac', '.m2a',
-    '.m4a', '.m4b', '.mp2', '.mp3', '.mpc', '.oga', '.ogg', '.opus',
-    '.spx', '.wma', '.wav', '.wv', '.wvp'],
+    '.m2a', '.m4a', '.mpc', '.m4b', '.mka', '.mp2', '.mp3', '.mpc', '.oga',
+    '.ogg', '.opus', '.spx', '.wma', '.wav', '.wv', '.wvp'],
   video: [
     '.avi', '.mp4', '.m4v', '.webm', '.mov', '.mkv', '.mpg', '.mpeg',
     '.ogv', '.webm', '.wmv'],
